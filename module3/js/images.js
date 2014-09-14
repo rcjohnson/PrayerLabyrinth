23b90e35ddeb0013a94528bff94a1fae35ea6800
RC Johnson <rc@wpengine.com>
2014-09-14 14:30:59 -0500

    instagram for mvince

--- module3/js/images.js
+++ module3/js/images.js
@@ -2,6 +2,24 @@ $(document).ready(function() {
 	showImages();
 })
 
+function getInstagramImages() {
+	var jqxhr = $.getJSON( "https://api.instagram.com/v1/users/30926734/media/recent/?client_id=dff7ea4f038d43c78ea89b23b19234ec&callback=?", function() {
+	  console.log( "success" );
+	})
+	  .done(function(data) {
+	    $.each( data.data, function( i, item ) {
+	    	console.log( "item " + item.images.standard_resolution.url  + " " + i );
+	        setTimeout(function() {changeImg(i, "", item.images.standard_resolution.url);}, i * 3000 + 4000);
+	    });
+	  })
+	  .fail(function() {
+	    console.log( "error" );
+	  })
+	  .always(function() {
+	    console.log( "complete" );
+	  });
+}
+
 function getImages(num) {
 	img = Math.floor(Math.random() * 8);
 	num = Math.ceil(Math.random() * 152);
@@ -18,12 +36,17 @@ function showImages() {
 	setTimeout(function() {showImg(3);},3500);
 	setTimeout(function() {showImg(6);},4000);
 
-	setTimeout(function() {setInterval(function() {getImages();}, 3000);}, 4000);
+	setTimeout(function() {setInterval(function() {getImages();}, 3000);}, 5000);
 	setTimeout(function() {scrollToForm();}, 15*1000);
+	getInstagramImages();
 }
 
-function changeImg(pos, img) {
-	$("#img"+pos).attr("src", "http://prayerlabyrinth.com/labyrinth/module3/img/" + img + ".jpg");
+function changeImg(pos, img, url) {
+	if (url) {
+		$("#img"+pos).attr("src", url);
+	} else {
+		$("#img"+pos).attr("src", "http://prayerlabyrinth.com/labyrinth/module3/img/" + img + ".jpg");
+	}
 	showImg(pos);
 }
 
